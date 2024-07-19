886727e60f714a0804fed2c399124e7d20b2c5cf
Saurav Singh <78152828+South-IN@users.noreply.github.com>
2024-07-19 18:44:18 +0530

    Update App.jsx

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -5,6 +5,26 @@ import Signup from "./pages/Signup";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 const App = () => {
+  const url = "https://mychef-backend.onrender.com/";
+  const reloadWebsite = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${
+            response.status
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  };
+
+  setInterval(reloadWebsite, 600000);
   const { user } = useAuthContext();
   return (
     <div className="App">
